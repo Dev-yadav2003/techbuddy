58b7b882075753ccdae3a7638bc0d916fd7c15f2
Dev-yadav2003 <devyadav.lfc.dtu@gmail.com>
2025-05-25 02:51:57 +0530

    add env var

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const initializeSocket = require("./utils/socket");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://techbuddy-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   })
